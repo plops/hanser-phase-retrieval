0d75c6e60df5e5be7330314840998eee4467aab4
m <martin@uffz.(none)>
2010-10-24 23:13:13 +0100

    get rid of lambda and D

--- memi.m
+++ memi.m
@@ -1,20 +1,11 @@
 % calculate in-focus image for one coherent beam
-
-% of specified direction
-% memi(phi,theta,tm,tl)
-% range of angles:
-% phi=-alpha,,alpha
-% theta=0,,alpha
-% example: vars; memi(0.alpha,rr(s)<20,rr(s)<20)
+% the direction of the beam specified direction is
+% given by kx and ky both can assume values in [-.5 .. .5]
+% example: vars; memi(0,-.5,rr(s)<20,rr(s)<20)
 
 function u5 = memi(kx,ky,tm,tl)
-  vars;
-  rx=xx(s)*D;
-  ry=yy(s)*D;
-  u0=exp(2*pi*1i.*(rx.*kx+ry.*ky)); % illumination
-  % tm=rr(s)<20; % mma transmission
+  u0=exp(2*pi*1i.*(kx.*xx(tm)+ky.*yy(tm))); % illumination
   u2=ft(u0.*tm); % lcos illumination
-  % tl=rr(s)<20; % lcos transmission
   u4=ft(u2.*tl); % bfp illumination
   u5=ft(u4); % field (simple lens instead of microscope)
 end
--- memiacc.m
+++ memiacc.m
@@ -1,14 +1,16 @@
 %%
 tic
-vars;
+s=[128 128]
 acc=newim([s 600])*1.0;
-ma=1/(2*D);
+ma=1/2;
 i=0;
 for kx=-ma:ma/10:ma
     for ky=-ma:ma/10:ma
         i=i+1;
         acc(:,:,i)=squeeze(acc(:,:,i-1))+...
-            abs(memi(kx,ky,rr(s)<20,rr(s)<30)).^2;
+            abs(memi(kx,ky,rr(s)<10,rr(s)<30)).^2;
     end
 end
 toc
+
+
