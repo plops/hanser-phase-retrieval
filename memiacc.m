f9c5f11b74e47fe45428c4311cef590d241a2b68
Martin Kielhorn <kielhorn.martin@gmail.com>
2015-11-10 11:19:09 +0100

    add screenshot

--- memiacc.m
+++ memiacc.m
@@ -1,7 +1,11 @@
 %% incoherent addition of images in focal plane
 tic
 s=[64 64];
+tm=exp(-((xx(s)-14).^2+yy(s).^2)/30); 
+tl=exp(-((xx(s)-2).^2+(yy(s)+4).^2)/100); % soft
+tl=sqrt((xx(s)-12).^2+(yy(s)+20).^2)<10 | rr(s)<4;
 angles=7;
+%%
 acc=newim([s (2*angles+1)^2+1],'dfloat');
 ma=1/2;
 i=0;
@@ -9,7 +13,7 @@ for kx=-ma:ma/angles:ma
     for ky=-ma:ma/angles:ma
         i=i+1;
         acc(:,:,i)=squeeze(acc(:,:,i-1))+...
-            abs(memi(kx,ky,rr(s)<8.3,rr(s)<30)).^2;
+            abs(memi(kx,ky,tm,tl)).^2;
     end
 end
 toc
